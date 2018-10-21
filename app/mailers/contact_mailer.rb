70c6d8645387825feec7a26b40c0465248b56118
Wasseem Bazbaz <wasseembazbaz@outlook.com>
2018-10-21 12:00:09 +0000

    Trigger the mailer from the controller file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ActionMailer::base
   default to: 'the.waswas@gmail.com'
   
-  def contact_email(name,email,comments)
+  def contact_email(name,email,body)
     @name = name
     @email = email
-    @comments = comments
+    @body = body
     
     mail(from:email,subject:'Contact form message')
   end
